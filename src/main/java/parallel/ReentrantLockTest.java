4bb19d1dd81fbb60321044e75ca52581646724d7
osswangxining <osswangxining@163.com>
2018-05-04 17:39:02 +0800

    reentrant lock

---
+++ src/main/java/parallel/ReentrantLockTest.java
@@ -0,0 +1,44 @@
+package parallel;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+  private ArrayList<Integer> arrayList = new ArrayList<Integer>();
+  private Lock lock = new ReentrantLock();
+
+  public static void main(String[] args) {
+    final ReentrantLockTest test = new ReentrantLockTest();
+
+    new Thread() {
+      public void run() {
+        test.insert(Thread.currentThread());
+      };
+    }.start();
+
+    new Thread() {
+      public void run() {
+        test.insert(Thread.currentThread());
+      };
+    }.start();
+  }
+
+  public void insert(Thread thread) {
+    if (lock.tryLock()) {
+      try {
+        System.out.println(thread.getName() + "得到了锁");
+        for (int i = 0; i < 5; i++) {
+          arrayList.add(i);
+        }
+      } catch (Exception e) {
+        // TODO: handle exception
+      } finally {
+        System.out.println(thread.getName() + "释放了锁");
+        lock.unlock();
+      }
+    } else {
+      System.out.println(thread.getName() + "获取锁失败");
+    }
+  }
+}
